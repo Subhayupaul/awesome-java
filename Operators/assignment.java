94bfafbc5354a6873f5c024f2268ca88e63644fc
Subhayupaul <121049596+Subhayupaul@users.noreply.github.com>
2023-09-23 18:00:15 +0530

    hi

---
+++ Operators/assignment.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class assignment {
+    public static void main(String[] args) {
+        int A = 10;
+        A +=10; //A = A + 10;
+        System.out.println(A);
+        int B = 5;
+        B *=20; //B = B * 20;
+        System.out.println(B);
+    }
+}
--- Operators/relational.java
+++ Operators/relational.java
@@ -2,6 +2,9 @@ package Operators;
 
 public class relational {
     public static void main(String[] args) {
-        
+        int a = 10;
+        int b = 10;
+        System.out.println(a == b);
+        System.out.println(a != b);
     }
 }
